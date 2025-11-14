adc5775f610902c984145fba1f89deb0eaf08291
cjkresslein <charlie.kresslein@gmail.com>
2025-11-13 22:01:21 -0500

    moved Logistic Regression to separate file, added roc plotting, and did debugging

--- GLM.py
+++ GLM.py
@@ -35,15 +35,13 @@ class GLM:
         self.w = np.zeros(x_size)
 
         i = 0
-        while(i<1000):
+        while(i<500):
 
             # find the difference between prediction and y_train
             prediction = self.link_function(np.dot(X_train, self.w))
             
             # find the gradient of likelihood (wrt each component in w)
             gradient =  X_train.T @ (Y_train-prediction)
-            # print(np.linalg.norm(prediction-Y_train))
-            print(np.linalg.norm(gradient))
 
             # check if the gradient is 0 (or near 0)
             if np.linalg.norm(gradient) < self.grad_norm:
@@ -52,7 +50,6 @@ class GLM:
             # gradient ascent
             self.w = self.w + self.lr * gradient
             i += 1
-        print(i)
 
         
     def predict(self, x):
@@ -61,78 +58,6 @@ class GLM:
     
 
 
-class LogisticRegression(GLM):
-
-    def __init__(self, lr=0.01, grad_norm=0.01):
-        super().__init__(lr, grad_norm)
-        self.b = None
-
-    def link_function(self, x):
-        x = x.astype(float)
-        return expit(x)
-
-    def add_intercept(self, X_train):
-        # add column of ones to x to train intercept
-        b = np.ones(np.size(X_train, axis=0)).reshape(-1, 1)
-        X_train = np.concatenate((X_train, b), axis=1)
-    
-    def gradient_ascent(self, X_train, Y_train):
-        # add intercept to X_train
-        self.add_intercept(X_train)
-
-        # call gradient ascent from GLM class
-        return super().gradient_ascent(X_train, Y_train)
-    
-    def predict(self, x):
-        self.add_intercept(x)
-        return super().predict(x)
-    
-    def tp_fp(self, x, y, threshold):
-        samples = x.size(axis=0)
-        tp = fp = tn = fn = 0
-        for i in range(samples):
-            # predict the sample
-            prediction = self.predict(x[i])
-
-            # classify each prediction as tp, fp, tn, fn
-            if prediction >= threshold:
-                if y[i]:
-                    tp += 1
-                else:
-                    fp += 1
-            else:
-                if y[i]:
-                    fn += 1
-                else:
-                    tn += 1
-
-        tpr = tp/(tp + fn)
-        fpr = fp/(fp + tn)
-        return [tpr, fpr]
-    
-    def roc(self, x, y):
-        tpr_fpr = []
-        for i in range(100):
-            tpr_fpr.append(self.tp_fp(x, y, i))
-        return tpr_fpr
-
-    def auc(self, x, y):
-        auc = 0
-        roc = self.roc(x, y)
-        for rates in roc:
-            auc += rates[0]
-        return auc
-    
-    def accuracy(self, predictions, y_test, threshold):
-        y_test = y_test.flatten()
-        test_size = len(y_test)
-        correct = 0
-        for p in predictions:
-            if p > threshold:
-                correct += 1
-        return correct/test_size
-        
-        
 
     
 
---
+++ Logistic_Regression.py
@@ -0,0 +1,109 @@
+from .GLM import GLM
+import numpy as np
+from scipy.special import expit
+import matplotlib.pyplot as plt
+
+class LogisticRegression(GLM):
+
+    def __init__(self, lr=0.01, grad_norm=0.01):
+        super().__init__(lr, grad_norm)
+        self.b = None
+
+    def link_function(self, x):
+        if type(x) is float:
+            return expit(x)
+        x = x.astype(float)
+        return expit(x)
+
+    # add column of ones to x to train intercept
+    def add_intercept(self, X_train):
+        # dimension == 1
+        if X_train.ndim == 1:
+            X_train = np.append(X_train, 1)
+            return X_train
+        
+        # dimension > 1
+        b = np.ones(np.size(X_train, axis=0)).reshape(-1, 1)
+        X_train = np.concatenate((X_train, b), axis=1)
+    
+    def gradient_ascent(self, X_train, Y_train):
+        # add intercept to X_train
+        self.add_intercept(X_train)
+
+        # call gradient ascent from GLM class
+        return super().gradient_ascent(X_train, Y_train)
+    
+    def predict(self, x):
+        self.add_intercept(x)
+        return super().predict(x)
+    
+    def tp_fp(self, x, y, threshold):
+        samples = len(x)
+        tp = fp = tn = fn = 0
+        for i in range(samples):
+            # predict the sample
+            prediction = self.predict(x[i])
+
+            # classify each prediction as tp, fp, tn, fn
+            if prediction >= threshold/100:
+                if y[i]:
+                    tp += 1
+                else:
+                    fp += 1
+            else:
+                if y[i]:
+                    fn += 1
+                else:
+                    tn += 1
+
+        tpr = tp/(tp + fn)
+        fpr = fp/(fp + tn)
+        return [tpr, fpr]
+    
+    def roc(self, x, y):
+        y = y.flatten()
+        tpr_fpr = []
+        for i in range(100):
+            rates = self.tp_fp(x, y, i)
+            tpr_fpr.append(rates)
+        return tpr_fpr
+
+    def auc(self, x, y):
+        auc = 0
+        roc = self.roc(x, y)
+        for rates in roc:
+            auc += rates[0]
+        return auc/100
+    
+    def plot_roc(self, x, y):
+        roc = self.roc(x, y)
+        
+        tpr = [tpr_fpr[0] for tpr_fpr in roc]
+        fpr = [tpr_fpr[1] for tpr_fpr in roc]
+        thresholds = [i for i in range(1, 100)]
+
+        plt.plot(fpr, tpr)
+        plt.xlabel("False Positive Rate")
+        plt.ylabel("True Positive Rate")  
+        plt.title("ROC")
+
+        # add threshold labels
+        for xi, yi, label in zip(fpr, tpr, thresholds):
+            plt.text(xi, yi, label, fontsize=9, ha='right', va='bottom')
+        plt.show()   
+
+    
+    def accuracy(self, predictions, y_test, threshold):
+        y_test = y_test.flatten()
+        test_size = len(y_test)
+        correct = 0
+        for i in range(len(predictions)):
+            if (predictions[i] >= threshold):
+                if (y_test[i] == 1):
+                    correct += 1
+            else:
+                if y_test[i] == 0:
+                    correct += 1
+        return correct/test_size
+        
+        
